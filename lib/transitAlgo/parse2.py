9df40ecca97837abdf7c09499915e67f486912cd
an6reww <an6reww@gmail.com>
2023-06-16 13:07:39 -0400

    change input fields form id to name

--- lib/transitAlgo/parse2.py
+++ lib/transitAlgo/parse2.py
@@ -1,6 +1,38 @@
+# about the program
+
+# generates the time to travel between two stations
+# required fields: start_station_name and end_station_name
+# returns: time in hours, minutes, and seconds
+
+
+
+
 import csv
 from datetime import datetime, timedelta
 import scipy.stats as stats
+
+id_to_name = {}
+name_to_id = {}
+
+# organized into a list of dictionaries
+    # the list represents the row and correpsonding fields in the dictionary are searchable
+
+    # read stop_times.txt file
+with open('stop_times.txt', 'r') as file:
+        stop_times_reader = csv.DictReader(file)
+        stop_times_data = list(stop_times_reader)
+
+    # read stops.txt file
+with open('stops.txt', 'r') as file:
+        stops_reader = csv.DictReader(file)
+        stops_data = list(stops_reader)
+
+    # Create a dictionary to map stop_id to stop_name
+id_to_name = {stop['stop_id']: stop['stop_name']
+                         for stop in stops_data}
+    
+    # export to csv file 
+name_to_id = {v: k for k, v in id_to_name.items()}
 
 def convert_minutes_to_datetime(minutes):
     # Convert minutes to timedelta object
@@ -18,22 +50,15 @@ def generate_delay():
     return stats.t.rvs(df=2, loc=2.18, scale=0.90, size=1)[0]
 
 def calculate_travel_time(start_station_id, end_station_id):
-    # organized into a list of dictionaries
-    # the list represents the row and correpsonding fields in the dictionary are searchable
+    
 
-    # read stop_times.txt file
-    with open('stop_times.txt', 'r') as file:
-        stop_times_reader = csv.DictReader(file)
-        stop_times_data = list(stop_times_reader)
+    keys = name_to_id.keys()
+    with open('station_name_to_id.csv','w',newline = '') as file:
+        writer = csv.DictWriter(file,fieldnames=keys)
+        writer.writeheader()
+        writer.writerow(name_to_id)
 
-    # read stops.txt file
-    with open('stops.txt', 'r') as file:
-        stops_reader = csv.DictReader(file)
-        stops_data = list(stops_reader)
-
-    # Create a dictionary to map stop_id to stop_name
-    stop_name_mapping = {stop['stop_id']: stop['stop_name']
-                         for stop in stops_data}
+    
 
     # Find relevant trips
     relevant_trips = []
@@ -64,13 +89,20 @@ def calculate_travel_time(start_station_id, end_station_id):
         minutes = (travel_time.seconds % 3600) // 60
         seconds = travel_time.seconds % 60
 
-        print(f"Travel time from {stop_name_mapping[start_station_id]} to {stop_name_mapping[end_station_id]}:")
+        print(f"Travel time from {id_to_name[start_station_id]} to {id_to_name[end_station_id]}:")
         print(f"Travel time: {hours} hours, {minutes} minutes, {seconds} seconds")
     else:
         print("Unable to calculate travel time for the specified stations.")
 
-# Example usage
-start_station_id = 'A02S'  # should be flutter input
-end_station_id = 'H11S'  # should be flutter input
 
-calculate_travel_time(start_station_id, end_station_id)
+# # Example usage
+# start_station_id = 'A02S'  # should be flutter input
+# end_station_id = 'H11S'  # should be flutter input
+
+start_station_name = 'Inwood-207 St'
+end_station_name = 'Far Rockaway-Mott Av'
+
+calculate_travel_time(name_to_id.get(start_station_name),name_to_id.get(end_station_name))
+
+
+# calculate_travel_time(start_station_id, end_station_id)
