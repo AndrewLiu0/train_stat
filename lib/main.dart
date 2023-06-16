038cddcc6b787ccfdc4a76ba6db92bfea679ae1a
Eric Lin <ericlin071906@gmail.com>
2023-06-16 14:00:19 -0400

    Co-authored-by: Dylan Ma <arandomdma@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: unused_local_variable
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:dropdown_search/dropdown_search.dart';
+
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
 void main() {
   //runApp(const MyApp());
   runApp(
@@ -7,12 +13,7 @@ void main() {
   );
 }
 
-// class TransitApp extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext build){
-//     return ; // return the completed widget
-//   }
-// }
+
 
 // ignore: must_be_immutable whatever that means
 class GetInputs extends StatelessWidget{
@@ -43,6 +44,41 @@ class GetInputs extends StatelessWidget{
 
 }
 
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = list.first;
+  Widget build(BuildContext context){
+     return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+  }
+
 
 
 class TestApp extends StatelessWidget{
@@ -51,6 +87,7 @@ class TestApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      /* This creates a dropdown */
       home: Scaffold(
         appBar: AppBar(title: const Text('Destination Time')),
         body: Center(
@@ -59,7 +96,8 @@ class TestApp extends StatelessWidget{
               SizedBox(height: 50),
               GetInputs("My Location"),
               SizedBox(height: 10),
-              GetInputs("Destination")
+              GetInputs("Destination"),
+              const DropdownButtonExample(),
             ]
           )
 
