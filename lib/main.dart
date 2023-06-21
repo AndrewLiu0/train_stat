6427e0f88d0868adc06c8339842fff9eb44c525d
Eric Lin <ericlin071906@gmail.com>
2023-06-21 00:42:35 -0400

    Reverted main to working code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:csv/csv.dart';
@@ -8,185 +9,116 @@ import 'package:collection/collection.dart';
 import 'dart:convert' show utf8;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-/*
-void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      _channel.sink.add(_controller.text);
-    }
-  }
-  
+final channel = WebSocketChannel.connect(
+  Uri.parse('wss://echo.websocket.events'),
+);
 
-StreamBuilder(
-  stream: channel.stream,
-  builder: (context, snapshot) {
-    return Text(snapshot.hasData ? '${snapshot.data}' : '');
-  },
-)
-*/
 String myLocation = "";
 String destination = "";
 
 void main() {
-  runApp(MaterialApp(
-    home: TestApp(),
-  ));
+  
+  runApp(
+    MaterialApp(
+      home: TestApp(),
+    )
+  );
 }
 
-class TestApp extends StatefulWidget {
+class TestApp extends StatefulWidget{
   @override
   _TestAppState createState() => _TestAppState();
 }
 
 class _TestAppState extends State<TestApp> {
-  final _channel = WebSocketChannel.connect(
-    Uri.parse('wss://echo.websocket.events'),
-  );
   String myLocation = "";
   String destination = "";
   List<String> stations = [];
 
-  /*
-  void initState() {
+
+
+  @override
+  void initState(){
     super.initState();
     loadStations();
   }
-  */
 
   Future<void> loadStations() async {
     final fileContents = await rootBundle.loadString('assets/stops.txt');
-    final csvTable = const CsvToListConverter().convert(fileContents);
+    final csvTable = CsvToListConverter().convert(fileContents);
 
     setState(() {
       stations = csvTable
           .where((row) => row.length >= 3)
           .map((row) => row[2] as String)
           .toList();
-      stations =
-          stations.whereIndexed((index, _) => (index - 1) % 3 == 0).toList();
+      stations = stations.whereIndexed((index, _) => (index - 1) % 3 == 0).toList();
     });
 
     stations.removeAt(0);
   }
 
-  bool _showButton = false;
-  void changeStatus() {
-    setState(() {
-      _showButton = !_showButton;
-    });
-  }
+  
+
 
   @override
-  Widget build(BuildContext context) {
-    final ButtonStyle flatButtonStyle = TextButton.styleFrom(
-      primary: Colors.black87,
-      minimumSize: const Size(88, 36),
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(2)),
-      ),
-    );
+  Widget build(BuildContext context){
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Transit App"),
-        ),
-        body: Padding(
-            padding: const EdgeInsets.all(60),
-            child: SizedBox(
-              height: 150,
-              child: ListView(
-                children: <Widget>[
-                  createDropdown("My Location", "myLocation"),
-                  const SizedBox(height: 20),
-                  createDropdown("Destination", "destination"),
-                  TextButton(
-                    /*
-                    style: ButtonStyle(
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.blue),
-                    ),
-                    */
-                    style: flatButtonStyle,
-                    onPressed: sendMessage,
-                    /* send the message to the websocket, then have it relay the message back */
-                    child: const Text('Calculate time between these stations'),
-                  ),
-                  StreamBuilder(
-                    stream: _channel.stream,
-                    builder: (context, snapshot) {
-                      if (_showButton) {
-                        return Text(snapshot.hasData ? '${snapshot.data}' : '');
-                      } else {
-                        return const Text("");
-                      }
-                    },
-                  ),
-                  /*
-                  _showButton
-                      ? const SizedBox.shrink()
-                      : TextButton(
-                          /* return the app to its original state */
-                          onPressed: changeStatus,
-                          child: const Text("Calculate a different route"),
-                        ),
-
-                */
-                ],
-              ),
-            )));
-  }
-
-  DropdownSearch<String> createDropdown(String label, String inputType) {
-    void _inputType(String? station) {
-      switch (inputType) {
-        case "myLocation":
-          myLocation = station as String;
-          break;
-        case "destination":
-          destination = station as String;
-          break;
-        default:
-          throw const FormatException("Invalid input type");
-      }
-    }
-
-    return DropdownSearch<String>(
-      popupProps: const PopupProps.menu(
-          showSelectedItems: true,
-          showSearchBox: true,
-          searchFieldProps: TextFieldProps(
-            cursorColor: Colors.blue, // can add other customization later
-          )),
-      items: stations,
-      dropdownDecoratorProps: DropDownDecoratorProps(
-          dropdownSearchDecoration: InputDecoration(
-              labelText: label, hintText: "Find your station ... ")),
-      onChanged: _inputType,
+      appBar: AppBar(
+        title: Text("Transit App"),
+        
+      ),
+      body:  Padding(
+        padding: const EdgeInsets.all(60),
+        child: SizedBox(
+          height: 150,
+          child: ListView(
+            children: [
+              createDropdown("My Location", "myLocation"),
+              const SizedBox(height: 20),
+              createDropdown("Destination", "destination"),
+              
+            ],
+          ),
+        )
+      )
     );
   }
 
-/*
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      _channel.sink.add(_controller.text);
+
+DropdownSearch<String> createDropdown(String label, String inputType) {
+  void _inputType(String? station) {
+    switch (inputType) {
+      case "myLocation":
+        myLocation = station as String;
+        break;
+      case "destination":
+        destination = station as String;
+        break;
+      default:
+        throw const FormatException("Invalid input type");
     }
   }
 
-  void dispose() {
-    _channel.sink.close();
-    _controller.dispose();
-    super.dispose();
-  }
-  */
-  void sendMessage() {
-    if (myLocation != "" && destination != "") {
-      _channel.sink.add(myLocation);
-      _channel.sink.add(destination);
-    }
-  }
 
-  void dispose() {
-    _channel.sink.close();
-    //_controller.dispose();
-    super.dispose();
-  }
+  return DropdownSearch<String>(
+    popupProps: const PopupProps.menu(
+      showSelectedItems:  true,
+      showSearchBox: true,
+      searchFieldProps: TextFieldProps(
+        cursorColor: Colors.blue,// can add other customization later
+      )
+    ),
+
+    items: stations,
+
+    dropdownDecoratorProps: DropDownDecoratorProps(
+      dropdownSearchDecoration: InputDecoration(
+        labelText: label,
+        hintText: "Find your station ... " 
+      )
+    ),
+    onChanged: _inputType,
+  );
+}
 }
